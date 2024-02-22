cfd7ffae4f638c00d7d742a8d2a451537ef54461
AlanResendizC <160932542+AlanResendizC@users.noreply.github.com>
2024-02-22 13:25:36 -0600

    Update README.c

--- README.c
+++ README.c
@@ -6,7 +6,7 @@ int main()
 {
 
 char opcion;
-float num1,num2;
+float num1,num2,res;
   
 //MENU
 printf("ELIGE UNA OPCION: +,-,*,/ ");
@@ -41,22 +41,8 @@ printf("operacion no valida");
 
 
 }
+
+   printf("Resultado de la operacion \n %f" res);
 return 0;
-}
 
-float mul(float num1, float num2){
-  float res;
-  res = num1 * num2;
-  return res;
-}
-
-float div(float num1, float num2){
-  float res;
-  if(num2 == 0){
-    printf("Division sobre 0");
-    break;
-  }
-  res = num1 / num2;
-  return res;
-  printf("Resultado de la operacion \n %f" res);
 }
